e08e4fde28db57d4cb404ff36115444019f3f2d0
VASANTH C <124522500+VA-SA-NT-H@users.noreply.github.com>
2023-11-05 21:32:26 +0530

    Update LINEAR_SEARCH.cpp

--- LINEAR_SEARCH.cpp
+++ LINEAR_SEARCH.cpp
@@ -9,3 +9,36 @@ public:
 void GetData(void);
 void SequentialSearch(void);
 };
+void Sequential::GetData(void)
+{
+cout<<"\nEnter the size of the list to be searched : ";
+cin>>n;
+cout<<"\nEnter the elements of the list : ";
+for(int i=0;i<n;i++)
+{
+ cin>>a[i];
+}
+cout<<"Enter the key value to be searched in the above list : ";
+cin>>key;
+}
+void Sequential::SequentialSearch(void)
+{
+for(int i=0;i<n;i++)
+{
+ if(a[i]==key)
+ {
+ cout<<"The key value "<<key<<" is present at location "<<i+1;
+ return;
+ }
+}
+cout<<"The key value "<<key<<" is not present in the above list";
+}
+int main()
+{
+Sequential S;
+clrscr();
+S.GetData();
+S.SequentialSearch();
+getch();
+return 0;
+}
